552f065bf764015a87bdcf939bbaf4a4eb8145c6
dreamjser <dreamjser@gmail.com>
2023-07-13 21:21:06 +0800

    feat: 删除注释

--- postcss.config.js
+++ postcss.config.js
@@ -6,14 +6,14 @@ module.exports = {
     "postcss-url": {},
     "autoprefixer": {},
     "postcss-px-to-viewport": {
-      unitToConvert: "px", // 要转化的单位
-      viewportWidth: 750, // UI设计稿的宽度
-      unitPrecision: 6, // 转换后的精度，即小数点位数
-      propList: ["*"], // 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
-      minPixelValue: 2, // 默认值1，小于或等于1px则不进行转换
-      mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
-      replace: true, // 是否转换后直接更换属性值
-      landscape: false // 是否处理横屏情况
+      unitToConvert: "px",
+      viewportWidth: 750,
+      unitPrecision: 6,
+      propList: ["*"],
+      minPixelValue: 2,
+      mediaQuery: true,
+      replace: true,
+      landscape: false
     }
   }
 }
