9a5e716190310839721943680a16adf3191e6dd2
dreamjser <dreamjser@gmail.com>
2023-07-27 11:04:52 +0800

    feat: 增加constant.ts

--- src/portal/app.tsx
+++ src/portal/app.tsx
@@ -2,9 +2,7 @@ import React, { FC, Suspense } from 'react'
 import { HashRouter, Routes, Route } from 'react-router-dom'
 import { Navigate } from 'react-router-dom'
 import children from '@tmp/routers'
-import appConfig from '../../app.config'
-
-const ROOT_REDIRECT = appConfig.rootRedirect
+import { ROOT_REDIRECT } from '@/common/constant'
 
 const lazyComponent = (Component: FC) => (
   <Suspense fallback={<></>}>
