dc73d3209b047ee09038937005a0db68e7529e06
dreamjser <dreamjser@gmail.com>
2023-07-29 20:18:57 +0800

    feat: 界面库

--- src/portal/app.tsx
+++ src/portal/app.tsx
@@ -2,7 +2,7 @@ import React, { FC, Suspense } from 'react'
 import { HashRouter, Routes, Route } from 'react-router-dom'
 import { Navigate } from 'react-router-dom'
 import children from '@tmp/routers'
-import { ROOT_REDIRECT } from '@/common/constant'
+import { ROOT_REDIRECT } from '@/common/utils/constant'
 
 const lazyComponent = (Component: FC) => (
   <Suspense fallback={<></>}>
