2080d621b5f840c985ad010848025962940b8686
dreamjser <dreamjser@gmail.com>
2023-08-01 00:44:14 +0800

    feat: 请求提示

--- src/common/request/index.ts
+++ src/common/request/index.ts
@@ -1,6 +1,5 @@
 import { getGlobalAxios, getAxios, AllType } from '@dreamjser/request-axios'
 import { showLoading, hideLoading } from './loading'
-import { Toast } from 'antd-mobile'
 
 const axiosInstance = getGlobalAxios({
   timeout: 30000,
@@ -15,14 +14,11 @@ const responseHook = (reslove: any, reject: any, res: any) => {
   const { config, data } = res
   const { errorCode, errorMsg } = data
 
-  !config.slient && setTimeout(hideLoading, 100)
+  !config.slint && setTimeout(hideLoading, 100)
 
   if (errorCode !== '0') {
     if (config.publicError) {
-      Toast.show({
-        icon: 'fail',
-        content: errorMsg,
-      })
+      App.interface.toast(errorMsg)
     } else {
       reject({
         errorCode,
@@ -39,10 +35,7 @@ const request = (opts: AllType) => {
   opts.requestHook = requestHook
   opts.responseHook = responseHook
   return getAxios(opts, axiosInstance).catch(() => {
-    Toast.show({
-      icon: 'fail',
-      content: '网络请求失败',
-    })
+    App.interface.toast('网络请求失败')
   })
 }
 
