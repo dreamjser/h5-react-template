d3c16e9d9c0283ae348ec7f0d3ee11594e6f608d
dreamjser <dreamjser@gmail.com>
2023-09-20 09:18:55 +0800

    feat: react state更新队列

--- src/modules/react_state/views/index/index.tsx
+++ src/modules/react_state/views/index/index.tsx
@@ -1,27 +1,45 @@
 import React, { useState } from 'react'
 import { Button } from 'antd-mobile'
+import { useImmer } from 'use-immer'
+
 const View = () => {
   type UserType = {
     name: string
     age: number
+    sex: string
   }
-  const [user, setUser] = useState<UserType>({
+  const [user, setUser] = useImmer<UserType>({
     name: '张三',
     age: 19,
+    sex: '男',
   })
+
+  const [number, setNumber] = useState(0)
 
   return (
     <>
       <div>姓名：{user.name}</div>
       <div>年龄：{user.age}</div>
+      <div>性别：{user.sex}</div>
+      <div>number: {number}</div>
       <div>
         <Button
           color="primary"
           onClick={() => {
-            setUser({
-              name: '李四',
-              age: 22,
+            setUser((user: UserType) => {
+              user.name = '孙元刚'
             })
+            setNumber(number + 1)
+            setNumber((n) => n + 1)
+            setNumber(number + 1)
+            setNumber((n) => n + 1)
+            // 更新队列中n的值，此时n为1，dom渲染结果为3
+            setNumber(number + 1)
+            setNumber((n) => n + 1)
+            setNumber((n) => n + 1)
+            setTimeout(() => {
+              console.log(user, '1s后')
+            }, 1000)
           }}
         >
           更新信息
